fcd6bfab8c8fdc678db56ff199369a50ccc55713
acostacos <33932255+acostacos@users.noreply.github.com>
2019-03-22 21:49:42 +0800

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -33,19 +33,84 @@ struct Program{
 	}
 };
 
+/*
+sort(p,p+n, [](const Node a, const Node b) -> bool{
+	// comparator logic
+});
+*/
 
 //SJFCompare class used when inserting Programs in the priority queue in the SJF Schduling method
 struct SJFCompare{
 public:
 	bool operator()(Program& p1, Program& p2){
-		//false: moves on to the next element, true: places element before current element
-		//p2 is the program being pushed
+		//false compares the next item in the queue, true inserts the program before the program it's currently being compared to
+		//cout << p1.index << " and " << p2.index << endl;
 
-		if(p1.arrival >= p2.arrival){
-
+		//check p1 is longer than p2
+		if(p1.burst > p2.burst){
+			return true;
 		}
+		else if(p1.burst == p2.burst){
+			if(p1.index > p2.index){
+				return true;
+			}
+		}
+
+		return false;
 	}
 };
+
+//returns number of array elements that can be added to the program
+int checkArrival(Program* arr, int size, int cp, int timer){
+	//check top of array and see if arrival time matches the current time
+	int counter = 0;
+	Program next = arr[cp];
+	//check the next things in the array until the arrival time is different
+	while(cp<size && next.arrival <= timer){
+		counter++;
+		next = arr[cp+counter];
+	}
+	//return number of elements to be added
+	return counter;
+}
+
+//helper function for quicksort
+void swap(Program* a, Program* b){
+	Program t = *a;
+	*a = *b;
+	*b = t;
+}
+
+//quickSort for the array, sort the array with the least arrival time first to the highest arrival time
+void quicksort(Program* arr, int low, int high){
+	Program pivot = arr[high];
+	int left = low;
+	int right = high-1;
+
+	while(left<right){
+		while(arr[left].arrival<pivot.arrival){
+			left++;
+		}
+		while(arr[right].arrival>pivot.arrival){
+			right--;
+		}
+		if(right<=left){
+			break;
+		}
+		swap(&arr[left],&arr[right]);
+		left++;
+		right--;
+	}
+
+	swap(&arr[left], &arr[high]);
+
+	if(left-1>low){
+		quicksort(arr, low, left-1);
+	}
+	if(left+1<high){
+		quicksort(arr, left+1, high);
+	}
+}
 
 /*
 FCFS
@@ -61,21 +126,28 @@ string sjf(Program* arr, int size){
 	int timer = 0;
 	int cp = 0;
 
-	//Sort array to make the least access time first and the greatest access time last
-
-	//Put the first element in the priority queue and increase timer by that amount
-
-	while(!pq.empty()){
+	//run while pq is not empty and while not all the programs have been added to the priority queue
+	do{
 		Program p = pq.top();
 		pq.pop();
 
-		while(p.getBurst()!=0){
+		cout << "Top: " << p.index << endl;
+
+		while(p.burst!=0){
 			//check if any of the processes enter with the timer
+			int progChecker = checkArrival(arr, size, cp, timer);
+			if(progChecker>0){
+				for(int i=0; i<progChecker; i++){
+					pq.push(arr[cp]);
+					cp++;
+				}
+			}
+
 			p.run();
 			timer++;
 		}
 		out << timer << " " << p.index << " " << p.runningTime << "X" << endl;
-	}
+	}while(cp<size || !pq.empty());
 
 	output = out.str();
 	return output;
@@ -112,15 +184,20 @@ int main(){
 	int numTest;
 	cin >> numTest;
 
+	cout << "Number of Test Cases: " << numTest << endl;
+
 	for(int i=0; i<numTest; i++){
 		int numPros; string type;
 		cin >> numPros;
 		cin >> type;
 
+		cout << "Number of Programs: " << numPros << endl;
+		cout << "Type of CPU Scheduling: " << type << endl;
+
 		if(type=="RR"){
 			int q;
 			cin >> q;
-		}
+		} 
 
 		Program list[numPros];
 
@@ -132,6 +209,13 @@ int main(){
 
 			Program p(j+1, arrival, burst, priority);
 			list[j] = p;
+		}
+
+		//Sort array to make the least access time first and the greatest access time last
+		quicksort(list, 0, numPros-1);
+
+		for(int j=0; j<numPros; j++){
+			cout << "Created Program: Index - " << list[j].index << ", Arrival - " << list[j].arrival << ", Burst - " << list[j].burst << ", Priority - " << list[j].priority << endl;
 		}
 
 		if(type=="SJF"){
