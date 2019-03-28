350f0483ef11cf966ceb1c19e184137949c40efe
Alfonso Dolina <alfonso.dolina@obf.ateneo.edu>
2019-03-28 22:54:46 +0800

    Prelim RR

--- Project1.cpp
+++ Project1.cpp
@@ -443,6 +443,91 @@ string pp(Program* arr, int size){
 	return output;
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------
+// RR
+//------------------------------------------------------------------------------------------------------------------------------------
+
+//RRCompare class used when inserting Programs in the priority queue in the RR Schduling method
+struct RRCompare{
+public:
+	bool operator()(Program& p1, Program& p2){
+		//false compares the next item in the queue, true inserts the program before the program it's currently being compared to
+		if(p1.arrival > p2.arrival){
+			return true;
+		}
+		else if(p1.arrival == p2.arrival){
+			if(p1.index > p2.index){
+				return true;
+			}
+		}
+
+		return false;
+	}
+};
+
+string rr(Program* arr, int size, int quanTime){
+	string output; stringstream out;
+	priority_queue<Program, vector<Program>, RRCompare> pq;
+	int timer = 0;
+	int cp = 0;
+
+	//push all of the elements that arrive at 0 inside
+	int initialChecker = checkArrival(arr, size, cp, timer);
+		if(initialChecker>0){
+			for(int i=0; i<initialChecker; i++){
+				pq.push(arr[cp]);
+				cp++;
+			}
+	}
+
+	//assignChecker is used to store whether a program was preemptively swapped or not. It only controls whether to discard the program, or put it back in the queue
+	bool switchChecker = false;
+	Program p;
+	//run while pq is not empty and while not all the programs have been added to the priority queue
+	//to check remaining time: burst-runningTime
+	while(cp<size || !pq.empty()){
+		if(switchChecker){
+			//set the currentTime to 0 if you put the process back in the queue
+			p.currentTime = 0;
+			pq.push(p);
+			switchChecker = false;
+		}
+
+		if(!pq.empty()){
+			p = pq.top();
+			pq.pop();
+		}
+
+		while(p.burst!=p.runningTime){
+			//check if any of the processes enter with the timer
+			int progChecker = checkArrival(arr, size, cp, timer);
+			if(progChecker>0){
+				for(int i=0; i<progChecker; i++){
+					pq.push(arr[cp]);
+					cp++;
+				}
+			}
+
+			//RR: Check if top of the queue has less remaining time left. If it does, make it the current running process
+			Program ptemp = pq.top();
+			if(ptemp.runningTime == quanTime){
+
+				switchChecker = true;
+				break;
+			}
+
+			p.run();
+			timer++;
+		}
+
+		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+
+	}
+
+	output = out.str();
+	return output;
+}
+
 /*
 PP, RR
 
@@ -474,10 +559,10 @@ int main(){
 		cin >> numPros;
 		cin >> type;
 
-		if(type=="RR"){
+		/*if(type=="RR"){
 			int q;
 			cin >> q;
-		} 
+		}*/
 
 		Program list[numPros];
 
@@ -498,12 +583,20 @@ int main(){
 			cout << "Created Program: Index - " << list[j].index << ", Arrival - " << list[j].arrival << ", Burst - " << list[j].burst << ", Priority - " << list[j].priority << endl;
 		}
 
+		if(type=="FCFS"){
+			cout << fcfs(list, numPros);
+		}
+
 		if(type=="SJF"){
 			cout << sjf(list, numPros);
 		}
 
 		if(type=="SRTF"){
 			cout << srtf(list, numPros);
+		}
+
+		if(type=="RR"){
+			cout << rr(list, numPros, 10);
 		}
 
 		if(type=="PN"){
