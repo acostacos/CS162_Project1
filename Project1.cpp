7fb07e073a49020cee8bd97b7fbfc359651fb341
Alfonso Dolina <alfonso.dolina@obf.ateneo.edu>
2019-03-31 15:34:36 +0800

    edited rrcompare a bit

--- Project1.cpp
+++ Project1.cpp
@@ -452,8 +452,10 @@ struct RRCompare{
 public:
 	bool operator()(Program& p1, Program& p2){
 		//false compares the next item in the queue, true inserts the program before the program it's currently being compared to
-		if(p1.arrival > p2.arrival && p1.runningTime > p2.arrival){
-			return true;
+		if(p1.arrival > p2.arrival && p1.runningTime >= p2.arrival){
+			if(p1.index > p2.index){
+				return true;
+			}
 		}
 		else if(p1.arrival == p2.arrival){
 			if(p1.index > p2.index){
@@ -493,7 +495,7 @@ string rr(Program* arr, int size, int quanTime){
 					cp++;
 				}
 			}
-			//cout << p.index << " " << p.runningTime << " " << p.burst << endl;
+
 			p.run();
 			timer++;
 			p.index++;
