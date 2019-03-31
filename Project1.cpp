abe074f474b509f8e32962bc9b3a30e438cdb128
acostacos <33932255+acostacos@users.noreply.github.com>
2019-03-31 23:30:02 +0800

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -14,7 +14,6 @@ using namespace std;
 //Program class which specifies one program
 struct Program{
 	//takes note of index, arrival time, burst time, priority and remaning time left to run
-	//current time represents how much time it spent running in a CPU burst (for preemptive scheduling purposes)
 	int index; int arrival; int burst; int priority;
 	int runningTime; int currentTime;
 
@@ -78,22 +77,6 @@ void quicksort(Program* arr, int low, int high){
 	}
 }
 
-//returns number of array elements that can be added to the program
-int checkArrival(Program* arr, int size, int cp, int timer){
-	//check top of array and see if arrival time matches the current time
-	int counter = 0;
-	Program next = arr[cp];
-	//check the next things in the array until the arrival time is different
-	while(cp+counter<size && next.arrival <= timer){
-		counter++;
-		next = arr[cp+counter];
-	}
-	//return number of elements to be added
-	return counter;
-}//SJFCompare class used when inserting Programs in the priority queue in the SJF Schduling method
-
-
-
 //------------------------------------------------------------------------------------------------------------------------------------
 // FCFS
 //------------------------------------------------------------------------------------------------------------------------------------
@@ -103,6 +86,7 @@ struct FCFSCompare{
 public:
 	bool operator()(Program& p1, Program& p2){
 		//false compares the next item in the queue, true inserts the program before the program it's currently being compared to
+		//check p1 is longer than p2
 		if(p1.arrival > p2.arrival){
 			return true;
 		}
@@ -121,35 +105,38 @@ string fcfs(Program* arr, int size){
 	priority_queue<Program, vector<Program>, FCFSCompare> pq;
 	int timer = 0;
 	int cp = 0;
+	Program p;
 
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
+	//run while pq is not empty and while not all the programs have been added to the priority queue
+	while(cp<size || !pq.empty()){
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
 				pq.push(arr[cp]);
 				cp++;
 			}
-	}
-	//run while pq is not empty and while not all the programs have been added to the priority queue
-	while(cp<size || !pq.empty()){
-		Program p = pq.top();
+		}
+
+		p = pq.top();
 		pq.pop();
 
 		while(p.burst!=p.runningTime){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int i=0; i<progChecker; i++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
 			}
-
+			
+			//keep running and increment the timer
 			p.run();
 			timer++;
 		}
 
-		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		out << timer << " " << p.index << " " << p.runningTime << "X" << endl;
 
 	}
 
@@ -185,36 +172,38 @@ string sjf(Program* arr, int size){
 	priority_queue<Program, vector<Program>, SJFCompare> pq;
 	int timer = 0;
 	int cp = 0;
-
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
-				pq.push(arr[cp]);
-				cp++;
-			}
-	}
+	Program p;
 
 	//run while pq is not empty and while not all the programs have been added to the priority queue
 	while(cp<size || !pq.empty()){
-		Program p = pq.top();
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
+				pq.push(arr[cp]);
+				cp++;
+			}
+		}
+
+		p = pq.top();
 		pq.pop();
 
 		while(p.burst!=p.runningTime){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int i=0; i<progChecker; i++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
 			}
-
+			
+			//keep running and increment the timer
 			p.run();
 			timer++;
 		}
 
-		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		out << timer << " " << p.index << " " << p.runningTime << "X" << endl;
 
 	}
 
@@ -250,22 +239,22 @@ string srtf(Program* arr, int size){
 	priority_queue<Program, vector<Program>, SRTFCompare> pq;
 	int timer = 0;
 	int cp = 0;
-
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
-				pq.push(arr[cp]);
-				cp++;
-			}
-	}
-
-	//assignChecker is used to store whether a program was preemptively swapped or not. It only controls whether to discard the program, or put it back in the queue
+	//switchChecker is used to store whether a program was preemptively swapped or not. It only controls whether to discard the program, or put it back in the queue
 	bool switchChecker = false;
 	Program p;
 	//run while pq is not empty and while not all the programs have been added to the priority queue
 	//to check remaining time: burst-runningTime
 	while(cp<size || !pq.empty()){
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
+				pq.push(arr[cp]);
+				cp++;
+			}
+		}
+
 		if(switchChecker){
 			//set the currentTime to 0 if you put the process back in the queue
 			p.currentTime = 0;
@@ -273,16 +262,13 @@ string srtf(Program* arr, int size){
 			switchChecker = false;
 		}
 
-		if(!pq.empty()){
-			p = pq.top();
-			pq.pop();
-		}
+		p = pq.top();
+		pq.pop();
 
 		while(p.burst!=p.runningTime){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int i=0; i<progChecker; i++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
@@ -291,7 +277,6 @@ string srtf(Program* arr, int size){
 			//Preemptive: Check if top of the queue has less remaining time left. If it does, make it the current running process
 			Program ptemp = pq.top();
 			if(ptemp.burst-ptemp.runningTime < p.burst-p.runningTime){
-
 				switchChecker = true;
 				break;
 			}
@@ -300,7 +285,11 @@ string srtf(Program* arr, int size){
 			timer++;
 		}
 
-		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		//used to handle a peculiar case. If a program arrives at the exact same cc that an interrupted program continues, it prints even if it was not able to run
+		//this error comes from the way the code was structured and could be improved later
+		if(p.currentTime!=0){
+			out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		}
 
 	}
 
@@ -341,36 +330,38 @@ string pn(Program* arr, int size){
 	priority_queue<Program, vector<Program>, PCompare> pq;
 	int timer = 0;
 	int cp = 0;
-
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
-				pq.push(arr[cp]);
-				cp++;
-			}
-	}
+	Program p;
 
 	//run while pq is not empty and while not all the programs have been added to the priority queue
 	while(cp<size || !pq.empty()){
-		Program p = pq.top();
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
+				pq.push(arr[cp]);
+				cp++;
+			}
+		}
+
+		p = pq.top();
 		pq.pop();
 
 		while(p.burst!=p.runningTime){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int i=0; i<progChecker; i++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
 			}
-
+			
+			//keep running and increment the timer
 			p.run();
 			timer++;
 		}
 
-		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		out << timer << " " << p.index << " " << p.runningTime << "X" << endl;
 
 	}
 
@@ -387,21 +378,22 @@ string pp(Program* arr, int size){
 	priority_queue<Program, vector<Program>, PCompare> pq;
 	int timer = 0;
 	int cp = 0;
-
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
-				pq.push(arr[cp]);
-				cp++;
-			}
-	}
-
-	//assignChecker is used to store whether a program was preemptively swapped or not. It only controls whether to discard the program, or put it back in the queue
+	//switchChecker is used to store whether a program was preemptively swapped or not. It only controls whether to discard the program, or put it back in the queue
 	bool switchChecker = false;
 	Program p;
 	//run while pq is not empty and while not all the programs have been added to the priority queue
+	//to check remaining time: burst-runningTime
 	while(cp<size || !pq.empty()){
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
+				pq.push(arr[cp]);
+				cp++;
+			}
+		}
+
 		if(switchChecker){
 			//set the currentTime to 0 if you put the process back in the queue
 			p.currentTime = 0;
@@ -409,16 +401,13 @@ string pp(Program* arr, int size){
 			switchChecker = false;
 		}
 
-		if(!pq.empty()){
-			p = pq.top();
-			pq.pop();
-		}
+		p = pq.top();
+		pq.pop();
 
 		while(p.burst!=p.runningTime){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int i=0; i<progChecker; i++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
@@ -435,7 +424,11 @@ string pp(Program* arr, int size){
 			timer++;
 		}
 
-		out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		//used to handle a peculiar case. If a program arrives at the exact same cc that an interrupted program continues, it prints even if it was not able to run
+		//this error comes from the way the code was structured and could be improved later
+		if(p.currentTime!=0){
+			out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+		}
 
 	}
 
@@ -470,25 +463,28 @@ string rr(Program* arr, int size, int quanTime){
 	priority_queue<Program, vector<Program>, RRCompare> pq;
 	int timer = 0;
 	int cp = 0;
+	Program p;
 
-	//push all of the elements that arrive at 0 inside
-	int initialChecker = checkArrival(arr, size, cp, timer);
-		if(initialChecker>0){
-			for(int i=0; i<initialChecker; i++){
+	//run while pq is not empty and while not all the programs have been added to the priority queue
+	while(cp<size || !pq.empty()){
+		if(pq.empty() && cp<size){
+			//Case just started: push all of the elements that arrive first inside
+			//Case while running: adjust timer to the next element to come that hasnt arrived yet and add it to the priority queue
+			timer=arr[cp].arrival;
+			while(arr[cp].arrival==timer && cp<size){
 				pq.push(arr[cp]);
 				cp++;
 			}
-	}
-	//run while pq is not empty and while not all the programs have been added to the priority queue
-	while(cp<size || !pq.empty()){
-		Program p = pq.top();
+		}
+
+		p = pq.top();
 		pq.pop();
 
+
 		for(int i = 0; i < quanTime; i++){
-			//check if any of the processes enter with the timer
-			int progChecker = checkArrival(arr, size, cp, timer);
-			if(progChecker>0){
-				for(int j=0; j<progChecker; j++){
+			//check if any of the processes enter with the timer and if they do add them
+			if(arr[cp].arrival==timer){
+				while(arr[cp].arrival==timer && cp<size){
 					pq.push(arr[cp]);
 					cp++;
 				}
@@ -496,23 +492,28 @@ string rr(Program* arr, int size, int quanTime){
 
 			p.run();
 			timer++;
-			p.index++;
+
 			if(p.runningTime == p.burst){
-				p.index = p.index - p.burst;
 				out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
 				break;
 			}
 		}
-		p.arrival = timer + (quanTime * pq.size()-1);
 
+		p.arrival = timer + (quanTime * pq.size());
+		
 		if(p.runningTime != p.burst){
-			out << timer << " " << p.index-p.currentTime << " " << p.currentTime << "X" << endl; 
+			//used so that it does not repeat print during the last index
+			if(!pq.empty()){
+				out << timer << " " << p.index << " " << p.currentTime << "X" << endl;
+				p.currentTime = 0; 
+			}
 			pq.push(p);
 		}
 	}
 	output = out.str();
 	return output;
 }
+
 
 //------------------------------------------------------------------------------------------------------------------------------------
 // MAIN FUNCTION
@@ -536,10 +537,10 @@ int main(){
 		cin >> numPros;
 		cin >> type;
 
-		/*if(type=="RR"){
-			int q;
+		int q;
+		if(type=="RR"){	
 			cin >> q;
-		}*/
+		} 
 
 		Program list[numPros];
 
@@ -572,16 +573,16 @@ int main(){
 			cout << srtf(list, numPros);
 		}
 
-		if(type=="RR"){
-			cout << rr(list, numPros, 10);
-		}
-
 		if(type=="PN"){
 			cout << pn(list, numPros);
 		}
 
 		if(type=="PP"){
 			cout << pp(list, numPros);
+		}
+
+		if(type=="RR"){
+			cout << rr(list, numPros, q);
 		}
 	}
 
